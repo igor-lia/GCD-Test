9f311117e0d9bc92b2fe4b9fe8b45d079686c93b
Ihor <56516292+degster55@users.noreply.github.com>
2022-07-27 23:28:41 +0300

    Worked with storyboard, created SecondViewController

---
+++ GCD-Test/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  GCD-Test
+//
+//  Created by Ігор Ляхович on 27.07.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GCD-Test/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GCD-Test
+//
+//  Created by Ігор Ляхович on 27.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var buttonLabel: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buttonLabel.layer.cornerRadius = 15
+    }
+}
+
